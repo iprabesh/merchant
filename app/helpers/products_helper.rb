1c96afe0e09bce2248bafb35887f0e5b3d39e0be
Prabesh <iprabesh@hotmail.com>
2018-07-31 10:57:28 -0500

    Complete Handling Stock

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,13 @@ module ProductsHelper
 	def print_price(price)
 		number_to_currency price
 	end
+
+	def print_stock(stock)
+		if stock.zero?
+			content_tag(:strong, "Out Of Stock")
+		else
+			"#{stock} aviliable"
+		end
+	end
+
 end
---
+++ db/migrate/20180731153659_add_stock_to_products.rb
@@ -0,0 +1,6 @@
+class AddStockToProducts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :stock, :integer, default: 0
+    
+  end
+end
